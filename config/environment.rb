e0e19edfc93a97adb19b48274231710e5592d9e2
Josue Acosta <acostajosued@gmail.com>
2016-01-27 00:20:17 +0000

    Added sendgrid again

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'radiant-hamlet-1763.herokuapp.com',
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
   :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
-ActionMailer::Base.delivery_method = :smtp
