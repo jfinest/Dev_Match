bf111728f321154e2cf8d9866eb4ed46f5ffdec2
Josue Acosta <acostajosued@gmail.com>
2016-02-17 21:29:31 +0000

    Fixed typo for whitelisting parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  before_filter :configure_permitted_paramaters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   
   protected
-    def configure_permitted_paramaters
+    def configure_permitted_parameters
       
-      devise_paramater_sanitizer.for(:sign_up) {|u| u.permit(:name, :stripe_card_token, :email, :password,
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :stripe_card_token, :email, :password,
       :password_confirmation) }
       
     end
