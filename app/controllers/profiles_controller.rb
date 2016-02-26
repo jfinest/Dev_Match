64600fa3fa11f461e4749975a1f41940dde210fd
Josue Acosta <acostajosued@gmail.com>
2016-02-26 01:53:59 +0000

    Added new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
-class Profiles < ApplicationController
-   
-   
+class ProfilesController < ApplicationController
     
+   def new
+      # form where each user can create own profile
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile
+   end
 end
